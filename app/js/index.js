e7940c76af11bfcd42b59f3c8109050c1e6e50be
Maksym Pavlushchenko <max.pavluschenko@gmail.com>
2018-09-03 01:35:17 +0300

    added animate scrolltop

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,13 @@
 document.querySelector(".video-overlay").onclick = function() {
 	this.style.display = "none";
 }
+
+type="text/javascript">
+ $(document).ready(function(){
+    $("#navbarNav").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 900);
+    });
+});
